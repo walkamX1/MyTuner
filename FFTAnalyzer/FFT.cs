2fac5fd6fbbb6ace5028adfc7fe778b04630d672
Andrey Chausov <dxaHTepb@gmail.com>
2017-04-18 02:34:53 +0300

    dxahtepb

---
+++ FFTAnalyzer/FFT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFTAnalyzer
+{
+    public class FFT
+    {
+        public static double[] calculateSpectr(double[] data)
+        {
+            return null;
+        }
+
+        private static int reverseBits(int x, int lg_n)
+        {
+            int ret = 0;
+            for (int i=0; i< lg_n; i++)
+            {
+                if ((x & (1<<i)) != 0)
+                {
+                    ret |= 1 << (lg_n - 1 + i);
+                }
+            }
+            return ret;
+        }
+    }
+}
