b49e2c9314a5edc2ebfe8b8520186fa66ec76593
Andrey Chausov <dxaHTepb@gmail.com>
2017-04-18 02:11:55 +0300

    не очень-то нужный Complex

---
+++ FFTAnalyzer/Complex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFTAnalyzer
+{
+    class Complex
+    {
+        public double im;
+        public double re;
+
+        public Complex(double _re)
+        {
+            im = 0;
+            re = _re;
+        }
+
+        public Complex(double _re, double _im)
+        {
+            re = _re;
+            im = _im;
+        }
+
+        public override string ToString()
+        {
+            if (im == 0)
+            {
+                return re.ToString();
+            }
+            return re + (im > 0 ? "+" : "") + im;
+        }
+    }
+}
