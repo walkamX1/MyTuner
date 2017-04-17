d9342924a83c6cc4e8ca0c311e2c0c1672f5cdf0
Andrey Chausov <dxaHTepb@gmail.com>
2017-04-18 01:59:21 +0300

    Empty project

--- MyTuner/Program.cs
+++ MyTuner/Program.cs
@@ -17,7 +17,6 @@ namespace MyTuner
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //Хуй
         }
     }
 }
