9d46f7dccfed4933e698a3ccf4bcc981dd8d6734
Andrey Chausov <dxaHTepb@gmail.com>
2017-04-18 19:14:27 +0300

    dxahtepb

--- FFTAnalyzer/FFT.cs
+++ FFTAnalyzer/FFT.cs
@@ -16,11 +16,11 @@ namespace FFTAnalyzer
         private static int reverseBits(int x, int lg_n)
         {
             int ret = 0;
-            for (int i=0; i< lg_n; i++)
+            for (int i = 0; i < lg_n; i++)
             {
                 if ((x & (1<<i)) != 0)
                 {
-                    ret |= 1 << (lg_n - 1 + i);
+                    ret |= 1 << (lg_n - 1 - i);
                 }
             }
             return ret;
